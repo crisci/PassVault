6f420a263f792ff2d5b68b95cf35064162ef3778
Luigi Crisci <l.crisci@reply.it>
2024-04-11 15:09:50 +0200

    feat: iced integration

--- src/main.rs
+++ src/main.rs
@@ -2,66 +2,110 @@
 // [] choose a device to store the sk
 // [] create the password (symmetric key) and encrypt the pk and sk stored on the USB
 // [] decrypt the sk and check if the format is correct (PEM)
-use std::{fs::{self, File}, io::{BufReader, Read, Write}, path::PathBuf};
+use iced::{
+    alignment::{self},
+    widget::{container, text},
+    Application, Command, Element, Length, Settings, Theme, font, 
+};
 
-use enums::KeyStatus;
-use openssl::{pkey::Private, rsa::Rsa};
-use rand::{thread_rng, Rng};
+use login::login;
 
+mod login;
 mod enums;
 
-fn main() {
-    let mut key = [0;64];
-    thread_rng().try_fill(&mut key).expect("Error to create the key");
-    let key_pair = Rsa::generate(2048).expect("Error generating the keys");
-    let private_key = private_key(key_pair).unwrap().extract();
-    if let Some(bytes) = private_key {
-        println!("{:?}", bytes);
-    }
+
+fn main() -> iced::Result {
+    ModalExample::run(Settings::default())
 }
 
-
-fn private_key(key_pair: Rsa<Private>) -> Result<KeyStatus, String>{
-    let dir = directories::BaseDirs::new().ok_or("Error getting base directories")?;
-    let new_dir = PathBuf::from(format!("{}/{}", dir.data_local_dir().to_str().ok_or("Error getting data local dir")?, "PassVault"));
-    let file_path = new_dir.join("private_key.pem");
-    let private_key = key_pair.private_key_to_pem().expect("Error private key");
-
-    if !new_dir.exists() {
-        fs::create_dir_all(&new_dir).map_err(|err| format!("Error creating directory: {}", err))?;
-        // First time creation
-        let mut file = File::create(&file_path).map_err(|err| format!("Error creating file: {}", err))?;
-        file.write_all(&private_key).map_err(|err| format!("Error writing to file: {}", err))?;
-        return Ok(KeyStatus::CREATED(private_key));
-    } else {
-        // File already exists, so read the file
-        let mut key = Vec::new();
-        let file = File::open(&file_path).map_err(|err| format!("Error opening file: {}", err))?;
-        let mut reader = BufReader::new(file);
-        reader.read_to_end(&mut key).map_err(|err| format!("Errore nella lettura del file: {}", err))?;
-        return Ok(KeyStatus::PRESENT(private_key));
-    }
+#[derive(Clone, Debug)]
+enum Message {
+    PasswordChanged(String),
+    ConfirmPasswordChanged(String),
+    SavePassword,
+    #[allow(dead_code)]
+    Loaded(Result<(), String>),
+    FontLoaded(Result<(), font::Error>),
 }
 
-fn public_key(key_pair: Rsa<Private>) -> Result<KeyStatus, String>{
-    let dir = directories::BaseDirs::new().ok_or("Error getting base directories")?;
-    let new_dir = PathBuf::from(format!("{}/{}", dir.data_local_dir().to_str().ok_or("Error getting data local dir")?, "PassVault"));
-    let file_path = new_dir.join("private_key.pem");
-    let key_pair = Rsa::generate(2048).expect("Error generating the keys");
-    let private_key = key_pair.private_key_to_pem().expect("Error private key");
+#[derive(Debug)]
+enum ModalExample {
+    Loading,
+    Loaded(State),
+}
 
-    if !new_dir.exists() {
-        fs::create_dir_all(&new_dir).map_err(|err| format!("Error creating directory: {}", err))?;
-        // First time creation
-        let mut file = File::create(&file_path).map_err(|err| format!("Error creating file: {}", err))?;
-        file.write_all(&private_key).map_err(|err| format!("Error writing to file: {}", err))?;
-        return Ok(KeyStatus::CREATED(private_key));
-    } else {
-        // File already exists, so read the file
-        let mut key = Vec::new();
-        let file = File::open(&file_path).map_err(|err| format!("Error opening file: {}", err))?;
-        let mut reader = BufReader::new(file);
-        reader.read_to_end(&mut key).map_err(|err| format!("Errore nella lettura del file: {}", err))?;
-        return Ok(KeyStatus::PRESENT(private_key));
+#[derive(Default, Debug)]
+pub struct State {
+    theme: Theme,
+    password: String,
+    confirm_password: String
+}
+
+async fn load() -> Result<(), String> {
+    Ok(())
+}
+
+impl Application for ModalExample {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = iced::executor::Default;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (ModalExample, Command<Message>) {
+        (
+            ModalExample::Loading,
+            Command::batch(vec![
+                font::load(iced_aw::BOOTSTRAP_FONT_BYTES).map(Message::FontLoaded),
+                Command::perform(load(), Message::Loaded),
+            ]),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("Modal example")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Message> {
+        match self {
+            ModalExample::Loading => {
+                if let Message::Loaded(_) = message {
+                    *self = ModalExample::Loaded(State::default())
+                }
+            }
+            ModalExample::Loaded(state) => {
+                match message {
+                    Message::PasswordChanged(password) => state.password = password,
+                    Message::ConfirmPasswordChanged(password) => state.confirm_password = password,
+                    Message::SavePassword => if state.password == state.confirm_password {println!("Equal!")} else {println!("Not Equal!")}
+                    _ => {}
+                }
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        match self {
+            ModalExample::Loading => container(
+                text("Loading...")
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .size(50),
+            )
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .center_x()
+            .into(),
+            ModalExample::Loaded(state) =>  login(state)
+            
+        }
+    }
+
+    fn theme(&self) -> Theme {
+        match self {
+            ModalExample::Loading => Theme::Light,
+            ModalExample::Loaded(state) => state.theme.clone(),
+        }
     }
 }
---
+++ src/utils.rs
@@ -0,0 +1,53 @@
+pub mod utils {
+    use std::{fs::{self, File}, io::{BufReader, Read, Write}, path::PathBuf};
+
+    use openssl::{pkey::Private, rsa::Rsa};
+
+    use crate::enums::KeyStatus;
+
+    
+pub fn private_key(key_pair: Rsa<Private>) -> Result<KeyStatus, String>{
+    let dir = directories::BaseDirs::new().ok_or("Error getting base directories")?;
+    let new_dir = PathBuf::from(format!("{}/{}", dir.data_local_dir().to_str().ok_or("Error getting data local dir")?, "PassVault"));
+    let file_path = new_dir.join("private_key.pem");
+    let private_key = key_pair.private_key_to_pem().expect("Error private key");
+
+    if !new_dir.exists() {
+        fs::create_dir_all(&new_dir).map_err(|err| format!("Error creating directory: {}", err))?;
+        // First time creation
+        let mut file = File::create(&file_path).map_err(|err| format!("Error creating file: {}", err))?;
+        file.write_all(&private_key).map_err(|err| format!("Error writing to file: {}", err))?;
+        return Ok(KeyStatus::CREATED(private_key));
+    } else {
+        // File already exists, so read the file
+        let mut key = Vec::new();
+        let file = File::open(&file_path).map_err(|err| format!("Error opening file: {}", err))?;
+        let mut reader = BufReader::new(file);
+        reader.read_to_end(&mut key).map_err(|err| format!("Errore nella lettura del file: {}", err))?;
+        return Ok(KeyStatus::PRESENT(private_key));
+    }
+}
+
+pub fn public_key(key_pair: Rsa<Private>) -> Result<KeyStatus, String>{
+    let dir = directories::BaseDirs::new().ok_or("Error getting base directories")?;
+    let new_dir = PathBuf::from(format!("{}/{}", dir.data_local_dir().to_str().ok_or("Error getting data local dir")?, "PassVault"));
+    let file_path = new_dir.join("private_key.pem");
+    let key_pair = Rsa::generate(2048).expect("Error generating the keys");
+    let private_key = key_pair.private_key_to_pem().expect("Error private key");
+
+    if !new_dir.exists() {
+        fs::create_dir_all(&new_dir).map_err(|err| format!("Error creating directory: {}", err))?;
+        // First time creation
+        let mut file = File::create(&file_path).map_err(|err| format!("Error creating file: {}", err))?;
+        file.write_all(&private_key).map_err(|err| format!("Error writing to file: {}", err))?;
+        return Ok(KeyStatus::CREATED(private_key));
+    } else {
+        // File already exists, so read the file
+        let mut key = Vec::new();
+        let file = File::open(&file_path).map_err(|err| format!("Error opening file: {}", err))?;
+        let mut reader = BufReader::new(file);
+        reader.read_to_end  (&mut key).map_err(|err| format!("Errore nella lettura del file: {}", err))?;
+        return Ok(KeyStatus::PRESENT(private_key));
+    }
+}
+}
